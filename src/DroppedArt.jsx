ea88c4b66dcbe5309bcc926e22f08d1de2b4d9ae
JWH <jwheneghan@hotmail.com>
2024-05-16 10:56:20 -0230

    Add styles for JokerPreview hover effect

--- src/DroppedArt.jsx
+++ src/DroppedArt.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import JokerPreview from "./JokerPreview";
+
 const DroppedArt = ({
   artSrc,
   artName,
@@ -10,12 +11,18 @@ const DroppedArt = ({
   dataFromDescription,
 }) => {
   const [isHovered, setIsHovered] = React.useState(false);
+  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
 
   return (
     <div
       className="hoverable-dropped-art"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
+      onMouseMove={handleMouseMove}
     >
       <img src={artSrc} alt={artName} />
       {isHovered && (
@@ -24,6 +31,7 @@ const DroppedArt = ({
           jokerEffect={dataFromDescription}
           jokerRarity={dataFromRarity}
           jokerCost={dataFromCost}
+          position={mousePosition} // Pass mouse position to JokerPreview
         />
       )}
     </div>
--- src/JokerPreview.jsx
+++ src/JokerPreview.jsx
@@ -6,9 +6,17 @@ import rare from "./assets/rarity/rare.png";
 import legendary from "./assets/rarity/legendary.png";
 import previewBackground from "./assets/preview_bg.png";
 import previewEffect from "./assets/preview_effect.png";
-const JokerPreview = ({ jokerName, jokerEffect, jokerRarity, jokerCost }) => {
+
+const JokerPreview = ({
+  jokerName,
+  jokerEffect,
+  jokerRarity,
+  jokerCost,
+  position,
+}) => {
   let rarityTitle = "Common";
   let rarityImg = common;
+
   const getInitRarity = () => {
     switch (jokerRarity) {
       case 1:
@@ -32,40 +40,35 @@ const JokerPreview = ({ jokerName, jokerEffect, jokerRarity, jokerCost }) => {
   getInitRarity();
 
   return (
-    <>
-      <div>
-        <JokerCostTab jokerCost={jokerCost} />
+    <div
+      className="joker-preview"
+      style={{
+        position: "fixed",
+        top: position.y + 10,
+        left: position.x + 10,
+      }}
+    >
+      <JokerCostTab jokerCost={jokerCost} />
+      <div
+        className="joker-preview-background"
+        style={{ backgroundImage: `url(${previewBackground})` }}
+      >
+        <h2 className="joker-preview-name">{jokerName}</h2>
         <div
-          className="joker-preview"
-          style={{
-            backgroundImage: `url(${previewBackground})`,
-            backgroundSize: "cover",
-          }}
+          className="joker-preview-effect-container"
+          style={{ backgroundImage: `url(${previewEffect})` }}
         >
-          <h2 className="joker-preview-name">{jokerName}</h2>
-          <div
-            className="joker-preview-effect-container"
-            style={{
-              backgroundImage: `url(${previewEffect})`,
-              backgroundSize: "cover",
-            }}
-          >
-            <p className="joker-preview-effect">{jokerEffect}</p>
-          </div>
-          <div className="joker-preview-rarity-container">
-            <h3
-              className="joker-preview-rarity"
-              style={{
-                backgroundImage: `url(${rarityImg})`,
-                backgroundSize: "cover",
-              }}
-            >
-              {rarityTitle}
-            </h3>
-          </div>
+          <p className="joker-preview-effect">{jokerEffect}</p>
+        </div>
+        <div
+          className="joker-preview-rarity-container"
+          style={{ backgroundImage: `url(${rarityImg})` }}
+        >
+          <h3 className="joker-preview-rarity">{rarityTitle}</h3>
         </div>
       </div>
-    </>
+    </div>
   );
 };
+
 export default JokerPreview;
