26d150fcf7b2cb88c518c064b154b6f5eb3fe523
JWH <jwheneghan@hotmail.com>
2024-06-11 12:41:16 -0230

    update sidepanel styles / fix bg assets

--- src/sections/SidePanel/BlockTab.jsx
+++ src/sections/SidePanel/BlockTab.jsx
@@ -1,9 +1,26 @@
 /* eslint-disable react/prop-types */
 import LogicBlock from "./LogicBlocks";
 const BlockTab = ({ blockArray, blockType }) => {
+  let background = null;
+  switch (blockType) {
+    case "context-block":
+      background = "rgb(156, 94, 0)";
+      break;
+    case "condition-block":
+      background = "rgb(30, 63, 49)";
+      break;
+    case "results-block":
+      background = "rgb(255, 0, 0)";
+      break;
+  }
   return (
     <>
-      <div className="block-container">
+      <div
+        className="block-container"
+        style={{
+          backgroundColor: background,
+        }}
+      >
         {blockArray.map((block) => {
           return (
             <LogicBlock
--- src/sections/SidePanel/SidePanel.jsx
+++ src/sections/SidePanel/SidePanel.jsx
@@ -13,31 +13,42 @@ function SidePanel() {
   const [key, setKey] = useState("home");
   return (
     <div className="tab-container">
-      <Tabs
-        id="controlled-tab"
-        activeKey={key}
-        onSelect={(k) => setKey(k)}
-        className="mb-3"
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          margin: "2%",
+          width: "95%",
+          height: "95%",
+        }}
       >
-        <Tab eventKey="home" title="Context">
-          <BlockTab
-            blockArray={contextBlocks}
-            blockType={ItemTypes.CONTEXTBLOCK}
-          />
-        </Tab>
-        <Tab eventKey="conditions" title="Conditions">
-          <BlockTab
-            blockArray={conditionBlocks}
-            blockType={ItemTypes.CONDITIONBLOCK}
-          />
-        </Tab>
-        <Tab eventKey="results" title="Results">
-          <BlockTab
-            blockArray={resultBlocks}
-            blockType={ItemTypes.RESULTSBLOCK}
-          />
-        </Tab>
-      </Tabs>
+        <Tabs
+          id="controlled-tab"
+          activeKey={key}
+          onSelect={(k) => setKey(k)}
+          className="mb-3"
+        >
+          <Tab eventKey="home" title="Context">
+            <BlockTab
+              blockArray={contextBlocks}
+              blockType={ItemTypes.CONTEXTBLOCK}
+            />
+          </Tab>
+          <Tab eventKey="conditions" title="Conditions">
+            <BlockTab
+              blockArray={conditionBlocks}
+              blockType={ItemTypes.CONDITIONBLOCK}
+            />
+          </Tab>
+          <Tab eventKey="results" title="Results">
+            <BlockTab
+              blockArray={resultBlocks}
+              blockType={ItemTypes.RESULTSBLOCK}
+            />
+          </Tab>
+        </Tabs>
+      </div>
     </div>
   );
 }
