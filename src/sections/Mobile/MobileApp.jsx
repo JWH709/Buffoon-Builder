ab79f5b994ba92030eceb4b88686a50d896a1356
JWH <jwheneghan@hotmail.com>
2024-07-29 10:25:33 -0230

    Fixes to CSS

--- src/sections/Mobile/MobileApp.jsx
+++ src/sections/Mobile/MobileApp.jsx
@@ -5,7 +5,6 @@ import LuaDownloader from "../BuildingSpace/LuaDownloader";
 import SidePanel from "../SidePanel/SidePanel";
 import JokerInfo from "../JokerInfo/JokerInfo";
 import ItemTypes from "../../config/ItemTypes";
-import { IMAGES } from "../../config/assetImports";
 
 const MobileApp = ({
   setLuaJokerEffect,
@@ -68,120 +67,116 @@ const MobileApp = ({
             width: "100%",
             height: "80%",
             fontFamily: "balatro",
-            backgroundImage: `url(${IMAGES.builderBackground})`,
-            imageRendering: "pixelated",
-            backgroundSize: " 100% 100%",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            overflow: "hidden",
           }}
         >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              width: "100%",
-              height: "50%",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
+          <div className="mobile-container">
             <div
               style={{
                 display: "flex",
                 flexDirection: "row",
+                width: "100%",
+                height: "50%",
                 justifyContent: "center",
                 alignItems: "center",
-                width: "50%",
-                height: "100%",
               }}
             >
-              <button
-                onClick={() => {
-                  switch (currentList) {
-                    case ItemTypes.CONTEXTBLOCK:
-                      setCurrentList(ItemTypes.RESULTSBLOCK);
-                      break;
-                    case ItemTypes.CONDITIONBLOCK:
-                      setCurrentList(ItemTypes.CONTEXTBLOCK);
-                      break;
-                    case ItemTypes.RESULTSBLOCK:
-                      setCurrentList(ItemTypes.CONDITIONBLOCK);
-                      break;
-                    default:
-                      setCurrentList(ItemTypes.CONTEXTBLOCK);
-                      break;
-                  }
-                }}
-              >
-                -
-              </button>
-              {currentList == ItemTypes.CONTEXTBLOCK && (
-                <BuldingList
-                  blockType={ItemTypes.CONTEXTBLOCK}
-                  updateLua={setContextLua}
-                />
-              )}
-              {currentList == ItemTypes.CONDITIONBLOCK && (
-                <BuldingList
-                  blockType={ItemTypes.CONDITIONBLOCK}
-                  updateLua={setConditionsLua}
-                />
-              )}
-              {currentList == ItemTypes.RESULTSBLOCK && (
-                <BuldingList
-                  blockType={ItemTypes.RESULTSBLOCK}
-                  updateLua={setResultsLua}
-                />
-              )}
-              <button
+              <div
                 style={{
-                  width: "5%",
-                  height: "90%",
-                }}
-                onClick={() => {
-                  switch (currentList) {
-                    case ItemTypes.CONTEXTBLOCK:
-                      setCurrentList(ItemTypes.CONDITIONBLOCK);
-                      break;
-                    case ItemTypes.CONDITIONBLOCK:
-                      setCurrentList(ItemTypes.RESULTSBLOCK);
-                      break;
-                    case ItemTypes.RESULTSBLOCK:
-                      setCurrentList(ItemTypes.CONTEXTBLOCK);
-                      break;
-                    default:
-                      setCurrentList(ItemTypes.CONTEXTBLOCK);
-                      break;
-                  }
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "50%",
+                  height: "100%",
                 }}
               >
-                +
-              </button>
+                <button
+                  onClick={() => {
+                    switch (currentList) {
+                      case ItemTypes.CONTEXTBLOCK:
+                        setCurrentList(ItemTypes.RESULTSBLOCK);
+                        break;
+                      case ItemTypes.CONDITIONBLOCK:
+                        setCurrentList(ItemTypes.CONTEXTBLOCK);
+                        break;
+                      case ItemTypes.RESULTSBLOCK:
+                        setCurrentList(ItemTypes.CONDITIONBLOCK);
+                        break;
+                      default:
+                        setCurrentList(ItemTypes.CONTEXTBLOCK);
+                        break;
+                    }
+                  }}
+                >
+                  -
+                </button>
+                {currentList == ItemTypes.CONTEXTBLOCK && (
+                  <BuldingList
+                    blockType={ItemTypes.CONTEXTBLOCK}
+                    updateLua={setContextLua}
+                  />
+                )}
+                {currentList == ItemTypes.CONDITIONBLOCK && (
+                  <BuldingList
+                    blockType={ItemTypes.CONDITIONBLOCK}
+                    updateLua={setConditionsLua}
+                  />
+                )}
+                {currentList == ItemTypes.RESULTSBLOCK && (
+                  <BuldingList
+                    blockType={ItemTypes.RESULTSBLOCK}
+                    updateLua={setResultsLua}
+                  />
+                )}
+                <button
+                  style={{
+                    width: "5%",
+                    height: "90%",
+                  }}
+                  onClick={() => {
+                    switch (currentList) {
+                      case ItemTypes.CONTEXTBLOCK:
+                        setCurrentList(ItemTypes.CONDITIONBLOCK);
+                        break;
+                      case ItemTypes.CONDITIONBLOCK:
+                        setCurrentList(ItemTypes.RESULTSBLOCK);
+                        break;
+                      case ItemTypes.RESULTSBLOCK:
+                        setCurrentList(ItemTypes.CONTEXTBLOCK);
+                        break;
+                      default:
+                        setCurrentList(ItemTypes.CONTEXTBLOCK);
+                        break;
+                    }
+                  }}
+                >
+                  +
+                </button>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "50%",
+                  height: "100%",
+                }}
+              >
+                <LuaDownloader
+                  jokerName={dataFromName}
+                  jokerEffect={luaJokerEffect}
+                  localVariables={luaLocals}
+                  tableInsert={luaTableInsert}
+                  image={image}
+                  currentTab={currentTab}
+                  setCurrentTab={setCurrentTab}
+                  isMobile={isMobile}
+                />
+              </div>
             </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                width: "50%",
-                height: "100%",
-              }}
-            >
-              <LuaDownloader
-                jokerName={dataFromName}
-                jokerEffect={luaJokerEffect}
-                localVariables={luaLocals}
-                tableInsert={luaTableInsert}
-                image={image}
-                currentTab={currentTab}
-                setCurrentTab={setCurrentTab}
-                isMobile={isMobile}
-              />
-            </div>
+            <SidePanel />
           </div>
-          <SidePanel />
         </div>
       )}
       {currentTab && (
